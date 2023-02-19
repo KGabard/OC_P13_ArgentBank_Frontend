0b28b3f20e9c0587867a6faaf93904498cee44b8
KGabard <kev.gabard@gmail.com>
2023-02-19 12:13:42 +0100

    Ajout du header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import argentBankLogo from '../assets/logos/argent-bank-logo.png'
+import userIcon from '../assets/icons/icon-circle-user.svg'
 
 function Header() {
   return (
-    <div className='header'>Header</div>
+    <header className="header">
+      <Link to={'/'}>
+        <img src={argentBankLogo} alt="Logo" className="header__logo" />
+      </Link>
+      <div className="header__sign-in-wrapper">
+        <img
+          src={userIcon}
+          alt="user icon"
+          className="header__sign-in-wrapper__logo"
+        />
+        <Link to={'/sign-in'} className="header__sign-in-wrapper__link">
+          Sign In
+        </Link>
+      </div>
+    </header>
   )
 }
 
